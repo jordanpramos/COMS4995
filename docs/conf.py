4d69abe52530321a700d8b53b8e95cdb4443f2a4
Jordan Ramos <jprwestview@gmail.com>
2020-12-22 17:31:40 -0500

    sphinx autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -30,13 +30,11 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_js']
+extensions = [
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# The master toctree document.
-master_doc = 'README.rst'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. Flick documentation master file, created by
-   sphinx-quickstart on Tue Dec 22 05:51:52 2020.
+   sphinx-quickstart on Tue Dec 22 17:29:45 2020.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
