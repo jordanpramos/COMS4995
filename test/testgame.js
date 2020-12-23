066712451dec2226a613fb4c30d7de6f60ce557f
Jordan Ramos <jprwestview@gmail.com>
2020-12-22 21:33:40 -0500

    unit tests

---
+++ test/testgame.js
@@ -0,0 +1,36 @@
+import { getRandomLoc } from './game.js';
+var min_loc = 0;
+var max_loc = 5;
+let loc = getRandomLoc(min_loc, max_loc);
+var smaller_loc = (loc < min_loc);
+var bigger_loc = (loc > max_loc);
+
+import { getRandomSize } from './game.js';
+var min_size = 0;
+var max_size = 5;
+let loc = getRandomSize(min_size, max_size);
+var smaller_size = (loc < min_size);
+var bigger_size = (loc > max_size);
+
+const { assert } = require('chai');
+const { describe, it } = require('mocha');
+
+describe('game', () => {
+    describe('getRandomLoc', () => {
+        it('should return type int between min and max bounds'), () => {
+            assert.typeOf(loc, 'int', `getRandomLoc() does not return an int`);
+            assert.equal(smaller_loc, false);
+            assert.equal(bigger_loc, false);
+        }
+    });
+});
+
+describe('game', () => {
+    describe('getRandomSize', () => {
+        it('should return type int between min and max bounds'), () => {
+            assert.typeOf(loc, 'int', `getRandomSize() does not return an int`);
+            assert.equal(smaller_size, false);
+            assert.equal(bigger_size, false);
+        }
+    });
+});
